25f24fa450006b7a0b78721cd22ef722631460e0
Rolf Kleef <rolf@drostan.org>
2022-03-30 16:15:30 +0200

    enhance: switch to using a Zotero group library
    Switch from reference collections in a personal Zotero library to a group library.
    
    - Makes it possible to collaborate on collections.
    - Separates a personal, note-keeping library from a published, curated library

--- docs/documentation_system/modules/zotero/examples/get-zoteros.sh
+++ docs/documentation_system/modules/zotero/examples/get-zoteros.sh
@@ -2,7 +2,8 @@
 # load a series of zotero collections for an Antora documentation component
 ZAPI=https://api.zotero.org
 ZSRC=users/12345 # <1>
-CSL=https://www.drostan.org/drostan.csl # <2>
+ZKEY="Authorization: Bearer xyzxyz" # <2>
+CSL=https://www.drostan.org/drostan.csl # <3>
 
 if [[ -e antora.yml && -e zotero ]]; then
   cat zotero | while read zget module partial tag; do
@@ -13,10 +14,18 @@ if [[ -e antora.yml && -e zotero ]]; then
       with_tag=
       p_tag=
     fi
-    echo "Getting $zget/items$with_tag and putting into $module/partials/$partial.html"
-    mkdir -p "modules/$module/partials" 
-    curl -sS "$ZAPI/$ZSRC/${zget}items?format=bib&linkwrap=1$p_tag&itemType=-attachment&style=$CSL" -o "modules/$module/partials/$partial.html"
+    echo "Getting ${zget}items$with_tag and putting into $module/partials/$partial.html"
+    mkdir -p "modules/$module/partials"
+    curl -sS -H "$ZKEY" "$ZAPI/$ZSRC/${zget}items?format=bib&linkwrap=1$p_tag&itemType=-attachment&style=$CSL" -o "modules/$module/partials/$partial.html"
     echo "Appending COinS info"
-    curl -sS "$ZAPI/$ZSRC/${zget}items?format=coins$p_tag&itemType=-attachment" >> "modules/$module/partials/$partial.html"
+    curl -sS -H "$ZKEY" "$ZAPI/$ZSRC/${zget}items?format=coins$p_tag&itemType=-attachment" >> "modules/$module/partials/$partial.html"
+
+    echo "Include references with:" # <4>
+    echo -e "\n[bibliography]"
+    echo -e "== Background references\n"
+
+    echo "++++"
+    echo "include::partial\$$partial.html[]"
+    echo -e "++++\n"
   done
 fi
